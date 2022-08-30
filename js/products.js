0a9bfe9a92eb19f0966dc00ef5f20f5f8fd2cac8
jimena <jimena.tort@hotmail.com>
2022-08-29 22:33:46 -0300

    Entregable 1 - punto 2
    Cargo la lista de la categoría autos

--- js/products.js
+++ js/products.js
@@ -1,76 +1,44 @@
-const URL = PRODUCTS_URL+"101"+EXT_TYPE;
-const productos = document.getElementById("autos");
-let autosArray = [];
+//const URL = PRODUCTS_URL+"101"+EXT_TYPE;
+const URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+//const productos = document.getElementById("autos");
+let productosArray = [];
 
-getJSONData(URL).then(function(data){
-    if (data.status === "ok"){
-        for (const products in data) {
-            if (data.hasOwnProperty.call(data, products)) {
-                const element = data[products];
-                
-            }
-        }
-        autosArray = data.products;
-        console.log(data);
-        productos.innerHTML = autosArray;
-    }
-});
+function setCatID(id) {
+    localStorage.setItem("catID", id);
+    window.location = "products.html"
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function showAutosList(){
+function showAutosList(){
 
     let htmlContent = "";
-    for(let i = 0; i < autosArray .length; i++){
-        let autos = autosArray[i];
+    for(let i = 0; i < productosArray.length; i++){
+        let auto = productosArray[i];
 
-        if (((minCount == undefined) || (minCount != undefined && parseInt(autos.productCount) >= minCount)) &&
-            ((maxCount == undefined) || (maxCount != undefined && parseInt(autos.productCount) <= maxCount))){
-
-            htmlContent += `
-            <div onclick="setCatID(${autos.id})" class="list-group-item list-group-item-action cursor-active">
+        htmlContent += `
+            <div onclick="setCatID(${auto.id})" class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col-3">
-                        <img src="${autos.imgSrc}" alt="${autos.description}" class="img-thumbnail">
+                        <img src="${auto.image}" alt="${auto.description}" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${autos.name}</h4>
-                            <small class="text-muted">${autos.productCount} artículos</small>
+                            <h4 class="mb-1">${auto.name} - ${auto.currency} ${auto.cost}</h4>
+                            <small class="text-muted">${auto.soldCount} vendidos</small>
                         </div>
-                        <p class="mb-1">${autos.description}</p>
+                        <p class="mb-1">${auto.description}</p>
                     </div>
                 </div>
-            </div>
+            </div> 
             `
-        }
-
-        productos.innerHTML = htmlContent;
-    }
+        document.getElementById("autos").innerHTML = htmlContent;
+    }     
 }
 
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(URL).then(function(resultobj){
-        if (resultobj.status === "ok"){
-            autosArray = resultobj.data
+    getJSONData(URL).then(function(resultObj){
+      if (resultObj.status === "ok"){
+        productosArray = resultObj.data.products
             showAutosList()
-            
         }
     });
-
-});*/
+});
